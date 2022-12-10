c28afc57b9bce992abb890ddea8c887a78487d86
Yushua <yushabakker1@gmail.com>
2022-12-10 09:36:21 +0100

    made day2 smaller

--- day_10/main_2.cpp
+++ day_10/main_2.cpp
@@ -9,11 +9,8 @@ int main(){
     std::ifstream infile("text_1.txt");
 
     std::string line;
-    int cycle = 0;
-    int position = 1;
-    int check = 20;
-    int loops = 0;
-    int score = 0;
+    int cycle = 0, position = 1, check = 20, loops = 0, score = 0;
+
     for (int x = 0; x < 40; x++){
         map[0][x] = ".";
     }
@@ -24,50 +21,28 @@ int main(){
         std::string exection;
         int increase;
         iss >> exection;
-        if (exection == "noop"){
-            loops = 1;
-            increase = 0;
-        }
-        else if (exection == "addx"){
-            loops = 2;
-            iss >> increase;
-        }
+        /* check the execution */
+        if (exection == "noop"){ loops = 1; increase = 0; }
+        else if (exection == "addx"){ loops = 2; iss >> increase; }
+        /* loop through the positions */
         for (int i = 0; i < loops; i++){
             if (cycle%40 == 0 && cycle > 0){
-                for (int x = 0; x < 40; x++){
-                    std::cout << map[0][x];
-                }
-                for (int x = 0; x < 40; x++){
-                    map[0][x] = ".";
-                }
+                for (int x = 0; x < 40; x++){ std::cout << map[0][x];}
+                for (int x = 0; x < 40; x++){ map[0][x] = "."; }
                 std::cout << "\n";
             }
             int pos = cycle%40;
-            if (position - 1 == pos || position == pos || position + 1 == pos){
-                map[y][pos] = "#";
-            }
-            else{
-                map[y][pos] = ".";
-            }
+            if (position - 1 == pos || position == pos || position + 1 == pos){ map[y][pos] = "#"; }
+            else{ map[y][pos] = "."; }
             if (cycle >= 20){
-                if (cycle == check || cycle == 20){
-                    check += 40;
-                    score += (cycle * position);
-                }
+                if (cycle == check || cycle == 20){ check += 40; score += (cycle * position); }
             }
             cycle += 1;
         }
-        if (cycle == check || cycle == 20){
-            check += 40;
-            score += (cycle * position);
-        }
+        if (cycle == check || cycle == 20){ check += 40; score += (cycle * position); }
         position += increase;
-        // std::cout << " X == " << position << std::endl;
     }
-    for (int x = 0; x < 40; x++){
-        std::cout << map[0][x];
-    }
-    std::cout << "\n\n";
-    std::cout << "score " << cycle << std::endl;
+    for (int x = 0; x < 40; x++){ std::cout << map[0][x]; }
+    std::cout << "\n\n" << "score " << cycle << std::endl;
     return (0);
 }
