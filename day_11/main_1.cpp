fc84d9bb0326a6e2c6c4a8f332aaa03a41d632e8
Yushua <yushabakker1@gmail.com>
2022-12-11 22:08:28 +0100

    gave up after this because its just boring

--- day_11/main_1.cpp
+++ day_11/main_1.cpp
@@ -27,10 +27,31 @@ int main(){
         iss >> a;
         if (a == "Monkey"){
             iss >> a;
+            a.pop_back();
             std::list<std::string> pair;
-            monkeys_used.insert(std::pair<std::string, long int>(a, 0));
+            monkeys_used.insert(std::pair<std::string, int>(a, 0));
             items_monkeys.insert(std::pair<std::string, std::list<std::string>>(a, pair));
+        }
+        else if (a == "Starting"){
+            std::list<std::string> items;
+            iss >> a;
+            while (iss >> a){
+                items.push_back(a);
+            }
         }
     }
     /* write it all out */
+    std::map<std::string, int>::iterator it;
+    for (it = monkeys_used.begin(); it != monkeys_used.end(); it++){
+        std::cout << "name: " << it->first << " used: " << it->second << std::endl;
+    }
+    std::map<std::string, std::list<std::string>>::iterator itt;
+    for (itt = items_monkeys.begin(); itt != items_monkeys.end(); itt++){
+        std::cout << "name: " << itt->first << " [";
+        for (std::string n : itt->second){
+            std::cout << n << " ";
+        }
+        std::cout << "]\n";
+    }
+    /* loop through for (std::string n : allDir) items_monkey->second*/
 }
